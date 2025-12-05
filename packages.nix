c780050965d9e52919a00e723e14757756efa523
Jusong Yu <jusong.d.yu@gmail.com>
2025-12-05 19:29:42 +0100

    packages.nix add devenv

--- packages.nix
+++ packages.nix
@@ -1,5 +1,6 @@
 # declarative package list for tools not available at arch/aur
 # the path added at end of $PATH
+# Run by: `nix-env -f packages.nix -i`
 {
   pkgs ? import <nixpkgs> { },
 }:
@@ -9,4 +10,5 @@ with pkgs;
 [
   nixd
   nixfmt-rfc-style
+  devenv
 ]
