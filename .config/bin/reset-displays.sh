2b9555b478fe9e0427d4254f70fdc33f2ffdc944
Jusong Yu <jusong.d.yu@gmail.com>
2025-12-11 23:47:24 +0100

    reset-display

--- .config/bin/reset-displays.sh
+++ .config/bin/reset-displays.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-swaymsg "output eDP-1 resolution 1920x1200 position 0 0 scale 1 transform normal"
+swaymsg "output eDP-1 enable resolution 1920x1200 position 0 0 scale 1 transform normal"
